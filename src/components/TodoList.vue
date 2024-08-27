c909a8e223328efb3fb9663be714de1a16353779
Bonjkh <perekotupolep@gmail.com>
2024-08-27 15:38:07 -0400

    First

---
+++ src/components/TodoList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>Todo List:</h1>
+    <form @submit.prevent="handleSubmit">
+      <label>Title:</label>
+      <input 
+        v-model="title"
+        placeholder="Write the Title..."
+      >
+      <label>Description:</label>
+      <input 
+        v-model="description"
+        placeholder="Write Todo's description..."
+      >
+      <button type="submit">Add</button>
+    </form>
+    <div v-for="todo in todos" :key="todo.id">
+      <h2>{{ todo.title }}</h2>
+      <p>{{ todo.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data() {
+    return {
+      title: '',
+      description: '',
+      todos: []
+    }
+  },
+  methods: {
+    handleSubmit() {
+      if(this.title.trim() || this.description.trim()) {
+        this.todos.push({
+          id: Date.now(),
+          title: this.title,
+          description: this.description
+        });
+        this.title = '';
+        this.description = '';
+      }
+    }
+  }
+}
+</script>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Home Page</h1>
+        <p>Welcome</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomePage'
+}
+</script>
